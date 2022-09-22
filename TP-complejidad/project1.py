537131821b936aca5403f4914e369bda592e9e38
Alvaro <u202015074@upc.edu.pe>
2022-09-22 09:44:04 -0500

    ud project

--- TP-complejidad/project1.py
+++ TP-complejidad/project1.py
@@ -37,3 +37,21 @@ class Grafo:
             line = line.rstrip()
             line = line.split(',')
             self.pushVertice(Hotel(line[0], line[1], float(line[2]), line[3]))
+
+    
+def main():
+    g = Grafo()
+    
+    print("d")
+
+
+###############################################################
+
+
+
+main()
+
+
+
+
+
