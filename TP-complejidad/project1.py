f59c22e4eda5de3ea951128783a29edbb940e6cc
Alvaro <u202015074@upc.edu.pe>
2022-09-24 15:56:41 -0500

    update-project
    configurate insert data

--- TP-complejidad/project1.py
+++ TP-complejidad/project1.py
@@ -1,55 +1,63 @@
 __author__ = 'ALVARO'
 
 class Hotel:
-    def __init__(self, id=0, name=" ", cost=0.0, deparment=" "):
-        self.id=id
-        self.name=name
+    def __init__(self, nombre=" ", cost=0.0,departamento=" ", vecino=" "):
+        self.nombre=nombre
         self.cost=cost
-        self.deparment=deparment
-#############################################################
-class Vertice:
-    def __init__(self, value=None):
-        self.value = value
-        self.node = []
-    
-    def pushNode(self, node=None):
-        if node!= None:
-            self.node.append(node)
-        else:
-            return
-    
-    def printNodes(self):
-        for i in range(len(self.node)):
-            print(self.node[i])
-#############################################################
+        self.departamento=departamento
+        self.vecino=vecino
+
+class Node:
+    def __init__(self, id=0, name=" ", cost=0.0, departamento=" ", vecino=" "):
+        self.id=id
+        self.value=Hotel(name, cost, departamento, vecino)
+        self.visited=False
+        self.nodes={}
+
+    def pushEdge(self, node:Hotel):
+        self.nodes[node.name] = node
+
 class Grafo:
     def __init__(self):
-        self.vertices = []
+        self.Nodes=[]
+        self.deparments = {"Lima": [], "Cuzco": [], "Huancayo": []}
+        #self.G=[]
     
-    def pushVertice(self, value):
-        if value.name not in self.vertices or len(self.vertices) < 1:
-            self.vertices.append(Vertice(value))
-
-    def readDocument(self, path):
-        fh = open(path)
-        lines = fh.readline()
-        for line in lines:
-            line = line.rstrip()
-            line = line.split(',')
-            self.pushVertice(Hotel(line[0], line[1], float(line[2]), line[3]))
-
+    def pushNode(self, value:Node):
+        self.Nodes.append(value)
     
+    def DesignGraf(self):
+        #Adicionando los nodos a los departamentos
+        for node in self.Nodes:
+            if node.value.vecino == "N":
+                if node is not self.deparments[node.value.departamento]:
+                    self.deparments[node.value.departamento].append(node)
+                else:
+                    continue
+            else:
+                continue
+        #Adicionando los nodos a los hoteles
+        for node in self.Nodes:
+            if node.value.vecino != "N":
+                
+
+
+
+
+
 def main():
-    g = Grafo()
-    
-    print("d")
+    path = "text/tp.csv"
+    file = open(path)
+    lines = file.readlines()
+    g=Grafo()
 
+    for line in lines:
+        line = line.rstrip()
+        line = line.split(',')
+        
+        #Creando uniones entre departamentos con hoteles
+        
 
-###############################################################
-
-
-
-main()
 
 
 
